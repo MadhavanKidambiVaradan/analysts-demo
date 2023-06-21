b925db9611a9d0ceb0ccd5b69802ef9d884cdfbb
MKI <mki@softwareag.com>
2023-06-21 21:16:25 +0530

    Changes made for the forrestor demo

---
+++ tests/performanceTest.js
@@ -0,0 +1,23 @@
+import { sleep } from"k6";
+import http from "k6/http";
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
+
+export let options = {
+  duration: "1m",
+  vus: 100,
+  thresholds: {
+	http_req_failed: ['rate<0.01'],
+    http_req_duration: ["p(10)<500"] // 95 percent of response times must be below 500ms
+  }
+};
+
+export default function() {
+  http.get(`https://analystsdemo.apigw-aw-eu.webmethods.io/gateway/Governed%20Petstore/1.0.6/pet/findByStatus?status=sold`)	
+  sleep(3);
+};
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
+}
