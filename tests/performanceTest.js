7e8a4947c3f901c0c3b4c868df6ad5422d69d054
MKI <mki@softwareag.com>
2023-06-29 17:10:22 +0530

    Petstore API change to the invokes.

--- tests/performanceTest.js
+++ tests/performanceTest.js
@@ -6,7 +6,7 @@ export let options = {
   duration: "1m",
   vus: 10,
   thresholds: {
-    http_req_duration: ["p(100)<500"] // 95 percent of response times must be below 500ms
+    http_req_duration: ["p(100)<1"] // 95 percent of response times must be below 500ms
   }
 };
 
